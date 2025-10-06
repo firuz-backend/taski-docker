0dae1ffb4524641d9cd48d0e4bcec8248e1a008a
Firuz Dadabaev <firuz.backend@gmail.com>
2025-10-07 00:12:32 +0500

    Saved changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhose', '127.0.0.1']
+ALLOWED_HOSTS = ['taski-yp1.sytes.net', '89.169.171.86', 'localhost', '127.0.0.1']
 
 
 # Application definition
